b251f917e8e24043e149e2d8f1e08570f325e2c9
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2015-03-23 19:30:10 +0200

    Commit

--- front/image_qr.php
+++ front/image_qr.php
@@ -1,13 +1,13 @@
 <?php
 include "../phpqrcode-master/qrlib.php";
 header('Content-type: image/png');
-if(!empty($_GET['cpu'])) {
+if(!empty($_GET['user'])) {
     if(isset($_GET['user']) && !empty($_GET['user']) ? $user = $_GET['user'] : $user = "-")
-    if(isset($_GET['cpu']) && !empty(trim($_GET['cpu'])) ? $cpu = $_GET['cpu'] : $cpu = "-")
-    if(isset($_GET['mb']) && !empty(trim($_GET['mb'])) ? $mb = $_GET['mb'] : $mb = "-")
-    if(isset($_GET['mem']) && !empty(trim($_GET['mem'])) ? $mem = $_GET['mem'] : $mem = "-")
-    if(isset($_GET['hdd']) && !empty(trim($_GET['hdd'])) ? $hdd = $_GET['hdd'] : $hdd = "-")
-    if(isset($_GET['os']) && !empty(trim($_GET['os'])) ? $os = $_GET['os'] : $os = "-")
+    if(isset($_GET['cpu']) && trim($_GET['cpu']) != "" ? $cpu = $_GET['cpu'] : $cpu = "-")
+    if(isset($_GET['mb']) && trim($_GET['mb']) != "" ? $mb = $_GET['mb'] : $mb = "-")
+    if(isset($_GET['mem']) && trim($_GET['mem']) != "" ? $mem = $_GET['mem'] : $mem = "-")
+    if(isset($_GET['hdd']) && trim($_GET['hdd']) != "" ? $hdd = $_GET['hdd'] : $hdd = "-")
+    if(isset($_GET['os']) && trim($_GET['os']) != "" ? $os = $_GET['os'] : $os = "-")
 
 
     $invent_text = <<<INVENT
