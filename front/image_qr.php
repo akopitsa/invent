55235e804627686777ac8af2901127c2e62edbc9
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2015-03-23 19:25:16 +0200

    Commit

--- front/image_qr.php
+++ front/image_qr.php
@@ -2,12 +2,12 @@
 include "../phpqrcode-master/qrlib.php";
 header('Content-type: image/png');
 if(!empty($_GET['cpu'])) {
-    if(isset($_GET['user']) ? $user = $_GET['user'] : $user = "-")
-    if(isset($_GET['cpu']) ? $cpu = $_GET['cpu'] : $cpu = "-")
-    if(isset($_GET['mb']) ? $mb = $_GET['mb'] : $mb = "-")
-    if(isset($_GET['mem']) ? $mem = $_GET['mem'] : $mem = "-")
-    if(isset($_GET['hdd']) ? $hdd = $_GET['hdd'] : $hdd = "-")
-    if(isset($_GET['os']) ? $os = $_GET['os'] : $os = "-")
+    if(isset($_GET['user']) && !empty($_GET['user']) ? $user = $_GET['user'] : $user = "-")
+    if(isset($_GET['cpu']) && !empty($_GET['cpu']) ? $cpu = $_GET['cpu'] : $cpu = "-")
+    if(isset($_GET['mb']) && !empty($_GET['mb']) ? $mb = $_GET['mb'] : $mb = "-")
+    if(isset($_GET['mem']) && !empty($_GET['mem']) ? $mem = $_GET['mem'] : $mem = "-")
+    if(isset($_GET['hdd']) && !empty($_GET['hdd']) ? $hdd = $_GET['hdd'] : $hdd = "-")
+    if(isset($_GET['os']) && !empty($_GET['os']) ? $os = $_GET['os'] : $os = "-")
 
 
     $invent_text = <<<INVENT
@@ -19,5 +19,5 @@ HDD: $hdd
 OS: $os
 INVENT;
     //QRcode::png($invent_text);
-    QRcode::png($invent_text, false, "H", 4, 4);
+    QRcode::png($invent_text, false, "H", 4,  4);
 }
