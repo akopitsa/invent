7eb423f1c532298abe966d0e819788759cb6dd54
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2015-03-23 19:11:01 +0200

    Commit

--- front/image_qr.php
+++ front/image_qr.php
@@ -2,12 +2,13 @@
 include "../phpqrcode-master/qrlib.php";
 header('Content-type: image/png');
 if(!empty($_GET['cpu'])) {
-    $user = $_GET['user'];
-    $cpu = $_GET['cpu'];
-    $mb = $_GET['mb'];
-    $mem = $_GET['mem'];
-    $hdd = $_GET['hdd'];
-    $os = $_GET['os'];
+    if(isset($_GET['user']) ? $user = $_GET['user'] : $user = "-")
+    if(isset($_GET['cpu']) ? $cpu = $_GET['cpu'] : $cpu = "-")
+    if(isset($_GET['mb']) ? $mb = $_GET['mb'] : $mb = "-")
+    if(isset($_GET['mem']) ? $mem = $_GET['mem'] : $mem = "-")
+    if(isset($_GET['hdd']) ? $hdd = $_GET['hdd'] : $hdd = "-")
+    if(isset($_GET['os']) ? $os = $_GET['os'] : $os = "-")
+
 
     $invent_text = <<<INVENT
 User: $user
