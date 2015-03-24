cbadff52074c8b5c68d21b345d40e7ed20547b60
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2015-03-24 14:35:34 +0200

    tuning

--- inc/user.class.php
+++ inc/user.class.php
@@ -2626,7 +2626,9 @@ class User extends CommonDBTM {
                           Dropdown::getDropdownName("glpi_plugin_genericobject_processormodels",$data["plugin_genericobject_processormodels_id"]).
                           Dropdown::getDropdownName("glpi_plugin_genericobject_operationsystemmodels",$data["plugin_genericobject_operationsystemmodels_id"]);
 
-
+                   if(Dropdown::getDropdownName("glpi_plugin_genericobject_hddtypes",$data["plugin_genericobject_hddtypes_id"]) != '&nbsp;') {
+                       $hdd .= " ".Dropdown::getDropdownName("glpi_plugin_genericobject_hddtypes",$data["plugin_genericobject_hddtypes_id"])." ";
+                   }
 
                    if (Dropdown::getDropdownName("glpi_plugin_genericobject_hddmodels",$data["plugin_genericobject_hddmodels_id"]) != '&nbsp;') {
                                 $hdd .=  Dropdown::getDropdownName("glpi_plugin_genericobject_hddmodels",$data["plugin_genericobject_hddmodels_id"]);
@@ -2667,9 +2669,8 @@ class User extends CommonDBTM {
                    if(Dropdown::getDropdownName("glpi_plugin_genericobject_memorietypes",$data["plugin_genericobject_memorietypes_id"]) != '&nbsp;') {
                        $mem .= " ".Dropdown::getDropdownName("glpi_plugin_genericobject_memorietypes",$data["plugin_genericobject_memorietypes_id"])." ";
                    }
-
-                   if(Dropdown::getDropdownName("glpi_plugin_genericobject_hddtypes",$data["plugin_genericobject_hddtypes_id"]) != '&nbsp;') {
-                       $hdd .= " ".Dropdown::getDropdownName("glpi_plugin_genericobject_hddtypes",$data["plugin_genericobject_hddtypes_id"])." ";
+                   if($data["size"] != '&nbsp;') {
+                       $hdd .= $data["size"];
                    }
 
 
